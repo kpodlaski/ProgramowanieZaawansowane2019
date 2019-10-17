533be61bca31c0b928f50641d0c6cd5ef36ce787
kpodlaski <podlaski@uni.lodz.pl>
2019-10-17 11:29:51 +0200

    wyszukiwanie danych w styczniu 2019 z danych kursów

--- zaj1/zaj2/pandas_ex.py
+++ zaj1/zaj2/pandas_ex.py
@@ -11,17 +11,21 @@ def proste_operacje(kursy):
     print(kursy.loc[row_id]['data'])
 
 
-
-kursy = pd.read_csv("../data/nbp_kursy_2019.csv"
-                    ,delimiter=";",
-                    encoding="cp1250",
-                    decimal=",",
-                    engine="python"
-                    )
+def count_avg_in_month(kursy, month):
+    #wyszukiwanie rekordów w stuczniu 2019
+    m_kursy = kursy.loc[kursy.data<'20190200']
+    m_kursy = m_kursy.loc.loc[kursy.data>'20190100']
+    print(m_kursy)
 
 if __name__=="__main__":
-    proste_operacje(kursy)
-
+    kursy = pd.read_csv("../data/nbp_kursy_2019.csv"
+                        , delimiter=";",
+                        encoding="cp1250",
+                        decimal=",",
+                        engine="python"
+                        )
+    #proste_operacje(kursy)
+    count_avg_in_month(kursy,1)
 
 ## Zadania do wykonania:
 ## Policzyć średnią w miesiącu (I-IX, 2019)
