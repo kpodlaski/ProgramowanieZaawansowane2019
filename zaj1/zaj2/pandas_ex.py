bdbf6664050f0b4a0b012b88a8bb81dfafed7b2e
kpodlaski <podlaski@uni.lodz.pl>
2019-10-17 11:04:01 +0200

    zadania zajecia 17.10.2019

--- zaj1/zaj2/pandas_ex.py
+++ zaj1/zaj2/pandas_ex.py
@@ -1,16 +1,28 @@
 import pandas as pd
 
+def proste_operacje(kursy):
+    print(kursy)
+    print(kursy.columns)
+    # print(kursy[['data','1USD']])
+    # print(kursy[['1USD']][0:-2].min())
+    usd = kursy['1USD'][2:-3].astype('float64')
+    print(usd)
+    row_id = usd.idxmin()
+    print(kursy.loc[row_id]['data'])
+
+
+
 kursy = pd.read_csv("../data/nbp_kursy_2019.csv"
                     ,delimiter=";",
                     encoding="cp1250",
                     decimal=",",
                     engine="python"
                     )
-print(kursy)
-print(kursy.columns)
-#print(kursy[['data','1USD']])
-#print(kursy[['1USD']][0:-2].min())
-usd = kursy['1USD'][2:-3].astype('float64')
-print(usd)
-row_id = usd.idxmin()
-print(kursy.loc[row_id]['data'])
+## Zadania do wykonania:
+## Policzyć średnią w miesiącu (I-IX, 2019)
+## Odchylenie standardowe w każdym miesiącu (I-IX, 2019)
+## Policzyć średnią kwartalną (QI,QII,QIII, 2019)
+## Odchylenie standardowe w każdym kwartale (QI,QII,QIII, 2019)
+## Który miesiąc  największe zmiany
+## Który kwartał najdroższy/najtanszy
+
